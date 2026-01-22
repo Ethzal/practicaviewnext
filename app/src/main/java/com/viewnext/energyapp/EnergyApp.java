1e01fcc7946ff044b1bf562993b7bc61d4f7a062
Ethzal <daniel.flores.jimenez.3a@gmail.com>
2026-01-22 09:55:17 +0100

    docs(app): add Javadoc to EnergyApp class
    - Added Javadoc to the EnergyApp class in the app module for clarity and maintainability.

--- app/src/main/java/com/viewnext/energyapp/EnergyApp.java
+++ app/src/main/java/com/viewnext/energyapp/EnergyApp.java
@@ -3,6 +3,11 @@ package com.viewnext.energyapp;
 import android.app.Application;
 import dagger.hilt.android.HiltAndroidApp;
 
+/**
+ * Clase principal de la aplicación para inicializar el framework de inyección de dependencias Hilt.
+ * Esta clase está anotada con @HiltAndroidApp para activar la generación de código de Hilt,
+ * lo que hace que la aplicación esté lista para la inyección de dependencias a lo largo del ciclo de vida de la app.
+ */
 @HiltAndroidApp
 public class EnergyApp extends Application {
 }
